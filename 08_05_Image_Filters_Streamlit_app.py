62c4246170ff9b7a36e2c3ca056a82e7da688dd6
Sergio Alexander Mondragón Silva <sergio.a.ms.usat@gmail.com>
2023-08-03 11:17:03 -0500

    github icon hide

--- 08_05_Image_Filters_Streamlit_app.py
+++ 08_05_Image_Filters_Streamlit_app.py
@@ -17,18 +17,14 @@ def get_image_download_link(img, filename, text):
 # Set title.
 st.title('Artistic Image Filters')
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+
+
+hide_github_icon ="""
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
+
+
 
 # Upload image.
 uploaded_file = st.file_uploader('Choose an image file:', type=['png','jpg'])
