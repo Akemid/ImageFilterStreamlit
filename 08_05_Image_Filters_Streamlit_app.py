bc76c8f44fd3944bf5c86ba3b8000e4de3b109e9
Sergio Alexander Mondragón Silva <sergio.a.ms.usat@gmail.com>
2023-08-03 11:21:17 -0500

    github icon hide

--- 08_05_Image_Filters_Streamlit_app.py
+++ 08_05_Image_Filters_Streamlit_app.py
@@ -20,7 +20,9 @@ st.title('Artistic Image Filters')
 
 
 hide_github_icon ="""
-.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+<style>
+ .css-ztfqz8.ef3psqc4 { display: none; }
+</style>
 """
 st.markdown(hide_github_icon, unsafe_allow_html=True)
 
